e4de6b041c77f12eb77a0cd29d55025c635fd1ef
myname21-meet <myname21@meet.mit.edu>
2019-07-30 05:43:15 -0400

    dfsv

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return "Hello World"
+	food=["pasta","kosa w wrk","pizza","hamburger"]
+	
+	opposite_day=False
+	least=["msh kosa"]
+	return render_template("index.html",foods=food,opposite_days=opposite_day,leasts=least)
 
 if __name__ == '__main__':
    app.run(debug = True)
+
